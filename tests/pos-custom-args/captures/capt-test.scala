026f1b71530219d1b9c7ff5aecd702957e467986
Martin Odersky <odersky@gmail.com>
2021-09-17 12:13:24 +0200

    change test

--- tests/pos-custom-args/captures/capt-test.scala
+++ tests/pos-custom-args/captures/capt-test.scala
@@ -1,8 +1,9 @@
 class C
 type Cap = {*} C
-def m[A, B]//: (f: {*} A => B) => {f} (List[A] => List[B])
-  =
-  //[A, B] =>
-    (f: {*} A => B) => (xs: List[A]) => f.asInstanceOf[List[B]]
+class A
+class B
+def m[A, B] =
+  ??? : ((f: {*} A => B) => {f} (xs: List[A]) => List[B])
+  //: (f: {*} A => B) => {f} (List[A] => List[B])
 
 def mc: (f: {*} String => Int) => {f} List[String] => List[Int] = m[String, Int]
