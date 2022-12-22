4f6eb18d9fa9acf2a17d22e1a9da31e3bfd130f5
odersky <odersky@gmail.com>
2022-12-22 12:30:01 +0100

    Don't have Diagnostic inherit from Exception
    Creating diagnostics should be cheap, whereas reportiong them can be expensive.
    The reason is that often diagnsotics are created nd then later discarded in normal
    backtracking during Typer.
    
    But the way it was set up, every diagnostic computed a stack trace, which is quite expensive.

--- compiler/src/dotty/tools/dotc/reporting/Diagnostic.scala
+++ compiler/src/dotty/tools/dotc/reporting/Diagnostic.scala
@@ -89,7 +89,7 @@ class Diagnostic(
   val msg: Message,
   val pos: SourcePosition,
   val level: Int
-) extends Exception with interfaces.Diagnostic:
+) extends interfaces.Diagnostic:
   private var verbose: Boolean = false
   def isVerbose: Boolean = verbose
   def setVerbose(): this.type =
@@ -102,5 +102,4 @@ class Diagnostic(
     msg.message.replaceAll("\u001B\\[[;\\d]*m", "")
 
   override def toString: String = s"$getClass at $pos: $message"
-  override def getMessage(): String = message
 end Diagnostic
