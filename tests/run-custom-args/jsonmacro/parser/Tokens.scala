869359ceb3eac9f90a6b74c235dfbaea5731365c
Nicolas Stucki <nicolas.stucki@gmail.com>
2023-01-31 22:17:38 +0100

    Refactor tokens

--- tests/run-custom-args/jsonmacro/parser/Tokens.scala
+++ tests/run-custom-args/jsonmacro/parser/Tokens.scala
@@ -11,7 +11,8 @@ private class Tokens(source: Seq[String]):
   private var nextToken: Token = null
 
   def peek(): Token =
-    if nextToken eq null then readNext()
+    if nextToken eq null then
+      nextToken = readToken()
     nextToken
 
   def next(): Token =
@@ -20,56 +21,59 @@ private class Tokens(source: Seq[String]):
       nextToken = null
     res
 
-  private def readNext(): Unit =
+  private def readToken(): Token =
     skipWhiteSpaces()
-    nextToken =
-      if offset == source(part).length then
-        if part == source.length - 1 then
-          offset += 1
-          Token.End
-        else
-          part += 1
-          offset = 0
-          Token.InterpolatedValue
-      else nextChar() match
-        case '{' => Token.OpenBrace
-        case '}' => Token.CloseBrace
-        case '[' => Token.OpenBracket
-        case ']' => Token.CloseBracket
-        case 'n' => accept("null", Token.Null)
-        case 'f' => accept("false", Token.False)
-        case 't' => accept("true", Token.True)
-        case ',' => Token.Comma
-        case ':' => Token.Colon
-        case '-' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'  =>
-          ??? // Num(value: String)
-        case '"' =>
-          boundary {
-            val stringBuffer = new collection.mutable.StringBuilder()
-            def parseChars(): String =
-              nextChar() match
-                case '"' => stringBuffer.result()
-                case '\\' =>
-                  nextChar() match
-                    case '\\' => stringBuffer += '\\'
-                    case '"' => stringBuffer += '"'
-                    case '/' => stringBuffer += '/'
-                    case 'b' => stringBuffer += '\b'
-                    case 'f' => stringBuffer += '\f'
-                    case 'n' => stringBuffer += '\n'
-                    case 'r' => stringBuffer += '\r'
-                    case 't' => stringBuffer += '\t'
-                    case 'u' => ??? // 4 hexadecimal digits
-                  parseChars()
-                case char if char.isControl =>
-                  boundary.break(Token.Error("unexpected control character", part, offset))
-                case char =>
-                  stringBuffer += char
-                  parseChars()
-            Token.Str(parseChars())
-          }
-        case _ =>
-          Token.Error("unexpected start of token", part, offset)
+    if offset == source(part).length then
+      if part == source.length - 1 then
+        offset += 1
+        Token.End
+      else
+        part += 1
+        offset = 0
+        Token.InterpolatedValue
+    else nextChar() match
+      case '{' => Token.OpenBrace
+      case '}' => Token.CloseBrace
+      case '[' => Token.OpenBracket
+      case ']' => Token.CloseBracket
+      case 'n' => accept("null", Token.Null)
+      case 'f' => accept("false", Token.False)
+      case 't' => accept("true", Token.True)
+      case ',' => Token.Comma
+      case ':' => Token.Colon
+      case '"' => readString()
+      case '-' => readNum('-')
+      case c if '0' <= c && c <= '9' => readNum(c)
+      case _ =>
+        Token.Error("unexpected start of token", part, offset)
+
+  private def readString(): Token =
+    boundary:
+      val stringBuffer = new collection.mutable.StringBuilder()
+      def parseChars(): String =
+        nextChar() match
+          case '"' => stringBuffer.result()
+          case '\\' =>
+            nextChar() match
+              case '\\' => stringBuffer += '\\'
+              case '"' => stringBuffer += '"'
+              case '/' => stringBuffer += '/'
+              case 'b' => stringBuffer += '\b'
+              case 'f' => stringBuffer += '\f'
+              case 'n' => stringBuffer += '\n'
+              case 'r' => stringBuffer += '\r'
+              case 't' => stringBuffer += '\t'
+              case 'u' => ??? // 4 hexadecimal digits
+            parseChars()
+          case char if char.isControl =>
+            boundary.break(Token.Error("unexpected control character", part, offset))
+          case char =>
+            stringBuffer += char
+            parseChars()
+      Token.Str(parseChars())
+
+  private def readNum(first: Char): Token =
+    ???
 
   private def accept(str: String, token: Token): Token =
     for char <- str.tail do
